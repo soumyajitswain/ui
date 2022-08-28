2ddbc4bc6bf448565561b74b94efb95b0be85282
soumyajit <soumyajit.swain@gmail.com>
2022-08-28 08:05:29 +0530

    Transaction progress is added....

--- environ_client/module/chargingStation.js
+++ environ_client/module/chargingStation.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import TransactionProgress from './transactionProgress';
 
-export default function ChargingStation({onPressChargeListPage}) {
-    return(
+export default function ChargingStation({ onPressChargeListPage }) {
+    const [chargingStarted, setChanrgingStarted] = useState(false);
+
+    const onPressStartCharging = () => {
+        alert('Charging started')
+        setChanrgingStarted(true);
+    }
+    const onPressBackChargeDetail = () => {
+        setChanrgingStarted(false);
+    }
+    return (
         <View>
-            <Text>Charging station detail</Text>
-            <TouchableOpacity>
-                <Text onPress={onPressChargeListPage}>LOGIN</Text>
-            </TouchableOpacity>
-
+            {!chargingStarted &&
+                <View>
+                    <Text>Charging station detail</Text>
+                    <TouchableOpacity>
+                        <Text onPress={onPressChargeListPage}>Back</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text onPress={onPressStartCharging}>Start</Text>
+                    </TouchableOpacity>
+                </View>
+            }
+            {chargingStarted &&
+                <TransactionProgress onPressBackChargeDetail={onPressBackChargeDetail}></TransactionProgress>
+            }
         </View>
     );
 }
--- environ_client/module/transactionProgress.js
+++ environ_client/module/transactionProgress.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function TransactionProgress() {
+export default function TransactionProgress({ onPressBackChargeDetail }) {
     return (
         <View>
-             <Text>Transaction Progress</Text>   
+            <Text>Transaction Progress</Text>
+            <TouchableOpacity>
+                <Text onPress={onPressBackChargeDetail}>Back</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text onPress={onPressBackChargeDetail}>Stop</Text>
+            </TouchableOpacity>
+
         </View>
     );
 }
