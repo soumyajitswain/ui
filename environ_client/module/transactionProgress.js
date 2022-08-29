bc20ab418e788c5abff07d430bea6bfcec60af55
soumyajit <soumyajit.swain@gmail.com>
2022-08-29 17:30:21 +0530

    progress bar added...

--- environ_client/module/transactionProgress.js
+++ environ_client/module/transactionProgress.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import CircularProgress from './components/CircularProgress';
 
 export default function TransactionProgress({ onPressBackChargeDetail }) {
     return (
         <View>
-            <Text>Transaction Progress</Text>
+            <CircularProgress percent={80}></CircularProgress>
             <TouchableOpacity>
                 <Text onPress={onPressBackChargeDetail}>Back</Text>
             </TouchableOpacity>
