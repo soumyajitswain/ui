5b1e4c9643b76e8e1ade4b808b6a3d4a41237f34
soumyajit <soumyajit.swain@gmail.com>
2022-08-31 18:09:17 +0530

    Payment page committed...

--- environ_client/module/payment.js
+++ environ_client/module/payment.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-export  default function payment() {
+export default function Payment() {
+    const makePayment = () => {
+        alert('Make Payment');
+    }
     return (
         <View>
-            <Text>Payment Page</Text>
+            <View style={[styles.container, {
+                backgroundColor: "grey",
+                flexDirection: "row"
+            }]}>
+                <Text>Unit consumed:</Text><Text>30</Text>
+            </View>
+            <View style={[styles.container, {
+                flexDirection: "row"
+            }]}>
+                <Text>Amount:</Text><Text>100</Text>
+            </View>
+            <TouchableOpacity>
+                <Text onPress={makePayment}>Payment</Text>
+            </TouchableOpacity>
         </View>
     )
+
 }
+const styles = StyleSheet.create(
+    {
+        container: {
+        }
+    }
+)
--- environ_client/module/transactionProgress.js
+++ environ_client/module/transactionProgress.js
@@ -1,18 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, ProgressViewIOSBase } from 'react-native';
 import CircularProgress from './components/CircularProgress';
+import Payment from './payment';
 
 export default function TransactionProgress({ onPressBackChargeDetail }) {
+    const [percent, setPercent] = useState(100);
+    const [openInvoicePage, setOpenInvoicePage] = useState(false);
+    const onPressOpenInvoicePage = () => {
+        setOpenInvoicePage(true);
+    }
+
     return (
         <View>
-            <CircularProgress percent={80}></CircularProgress>
-            <TouchableOpacity>
-                <Text onPress={onPressBackChargeDetail}>Back</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text onPress={onPressBackChargeDetail}>Stop</Text>
-            </TouchableOpacity>
+            {percent != 100 &&
+                <View>
+                    <CircularProgress percent={percent}></CircularProgress>
+                    <TouchableOpacity>
+                        <Text onPress={onPressBackChargeDetail}>Back</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text onPress={onPressBackChargeDetail}>Stop</Text>
+                    </TouchableOpacity>
+                </View>
+            }
+            {percent == 100 &&
+               <Payment></Payment>
+            }
+
 
         </View>
     );
